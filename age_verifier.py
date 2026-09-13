22998bc2bc6afa43c3da70a0b1b52c6b3209c4fa
joaolima07sec <jaomanuca@gmail.com>
2026-09-13 10:44:39 -0300

    Refactor age_verifier function and input handling
    Fixed syntax errors and improved input prompts. Updated the way user data is logged in the list.

--- age_verifier.py
+++ age_verifier.py
@@ -1,24 +1,25 @@
 from datetime import datetime
 def age_verifier():
   while True:
-     name = (input('what's your name?: '))
-     verifier = (input('you typed the name correctly? (y/n)'))
-     if verifier == "y" or verifier == "Y";
-       break;
-   while True:
-     try:
-       birthdate = int(input('what's the year you born?: '))
-       age = datetime.now().year - birthdate
-       break;
-     except ValueError:
-       print('type a valid year')
-  
-   if age < 18:
-     print ('you are underage\n')
+    name = input("what's your name?: ")
+    verifier = input("you typed the name correctly? (y/n)")
+    if verifier == "y" or verifier == "Y":
+      break
+
+  while True:
+    try:
+      birthdate = int(input("what's the year you born?: "))
+      age = datetime.now().year - birthdate
+      break
+    except ValueError:
+      print("type a valid year")
+
+  if age < 18:
+    print('you are underage\n')
   elif age > 130:
-     print ('are you in a casket?\n')
+    print('are you in a casket?\n')
   else:
-   print (f'your name is {name} and your age is {age}\n')
+    print(f'your name is {name} and your age is {age}\n')
   return name, age
 
 my_list = []
@@ -26,9 +27,12 @@ users = 0
 
 while True:
    name, age = age_verifier()
-   people = (inputI('wanna log more people? (y/n)\n')
+   people = input("wanna log more people? (y/n)\n")
 
-   my_list.append((name, age))
+   my_list.append(('name:', name, 'age:', age))
    users = users + 1 
-   if people == 'y' or people == 'S';
-     break;
+   if people == 'n' or people == 'N':
+     break
+
+print (users)
+print (my_list)
